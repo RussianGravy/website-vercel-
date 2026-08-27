85844cd04b3fc90280ed020a06b103566122e121
RussianGravy <valentinejones1215@gmail.com>
2026-08-27 11:21:33 -0700

    made experience data dynamic from json and added icons

--- src/components/ExperienceGraph.jsx
+++ src/components/ExperienceGraph.jsx
@@ -1,41 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Calendar, icons } from "lucide-react";
 import "./ExperienceGraph.css";
 
+function kebabToPascalCase(str) {
+  return str
+    .split("-")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join("");
+}
+
 export default function ExperienceGraph() {
+  const [experiences, setExperiences] = useState([]);
+  var evenNode = false;
+
+  useEffect(() => {
+    fetch("/experiences.json")
+      .then((response) => response.json())
+      .then((data) => setExperiences(data));
+  }, []);
+
   return (
     <div className="graph-body">
       <div className="connector-line"></div>
-      <Node
-        company="Stitch"
-        duration="Jan. 2025 to March 2025"
-        title="Intern"
-      />
-      <Node
-        company="ACM"
-        duration="Aug. 2024 to Present"
-        title="Software Engineer"
-        even="yes"
-      />
-      <Node
-        company="Learn2Code"
-        duration="Jan. 2024 to June 2024"
-        title="Instructor"
-      />
+      {experiences.map((exp) => {
+        evenNode = !evenNode;
+        return (
+          <Node
+            company={exp.company}
+            duration={exp.duration}
+            title={exp.title}
+            icon={exp.icon}
+            even={evenNode}
+          />
+        );
+      })}
     </div>
   );
 }
 
-function Node({ company, title, duration, even }) {
+function Node({ company, title, duration, icon, even }) {
+  const BulbIcon = icons[kebabToPascalCase(icon)];
+
   return (
     <div className="node">
-      <div className="bulb"></div>
+      <div className="bulb">
+        {BulbIcon && <BulbIcon size={24} color="white" />}
+      </div>
       <div className={"text-box " + (even ? "even" : "")}>
-        <p>
-          <bold>
-            <b>{company}</b>
-          </bold>
-          , {title}
-          <br />
+        <p className="company">
+          <b>{company}</b>
+        </p>
+        <p className="title">{title}</p>
+        <p className="duration">
+          <Calendar size={16} className="calendar-icon" />
           {duration}
         </p>
       </div>
